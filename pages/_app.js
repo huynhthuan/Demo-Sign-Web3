8798bc7e15d919c9bc9e93413fe609c7ef5bd886
huynhthuan <huynhthuanvugia@gmail.com>
2022-04-01 13:45:39 +0700

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Mainnet, DAppProvider, useEtherBalance, useEthers, Config } from '@usedapp/core';
+import { formatEther } from '@ethersproject/units'
+import { getDefaultProvider } from 'ethers'
+
+const config = {
+    readOnlyChainId: Mainnet.chainId,
+    readOnlyUrls: {
+        [Mainnet.chainId]: getDefaultProvider(),
+    },
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <DAppProvider config={config}>
+            <Component {...pageProps} />
+        </DAppProvider>
+    );
 }
 
-export default MyApp
+export default MyApp;
